523b175470705bd8b07bc5b7c9b75f65d7e8e3c5
Mehedi-Hassan <mehedii.hassan01@gmail.com>
2022-06-07 10:53:00 +0600

    firebase added sign in and sign up activity

--- app/src/main/java/com/example/tourmatenewproject/ui/LoginActivity.java
+++ app/src/main/java/com/example/tourmatenewproject/ui/LoginActivity.java
@@ -1,16 +1,25 @@
 package com.example.tourmatenewproject.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.tourmatenewproject.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+    private FirebaseAuth mAuth;
+    //private androidx.appcompat.app.AlertDialog.Builder alertDialogBuilder;
+    //private androidx.appcompat.app.AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +28,11 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        mAuth = FirebaseAuth.getInstance();
+
         binding.btnUserLogin.setOnClickListener(view -> {
-            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            userLogin();
+            //startActivity(new Intent(LoginActivity.this, MainActivity.class));
             finish();
         });
 
@@ -28,6 +40,58 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
+    }
+
+    private void userLogin() {
+        String email = binding.etUserEmail.getText().toString().trim();
+        String password = binding.etUserEmailPass.getText().toString().trim();
+
+
+        //checking the validity of the email
+        if (email.isEmpty()) {
+            //etSignUpUserEmail.setError("Enter your email address");
+            Toast.makeText(this, "Please enter your email address", Toast.LENGTH_SHORT).show();
+            binding.etUserEmail.requestFocus();
+            return;
+        }
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            //etSignUpUserEmail.setError("Enter a valid email address");
+            Toast.makeText(this, "Enter a valid email address", Toast.LENGTH_SHORT).show();
+            binding.etUserEmail.requestFocus();
+            return;
+        }
+
+        //checking the validity of the password
+        if (password.isEmpty()) {
+            //Toast.makeText(this, "Enter your password", Toast.LENGTH_SHORT).show();
+            binding.etUserEmailPass.setError("Enter your password");
+            binding.etUserEmailPass.requestFocus();
+        }
+
+        if (password.length() < 6) {
+            Toast.makeText(this, "wrong password", Toast.LENGTH_SHORT).show();
+            //etSignInUserPassword.setError("wrong password");
+            binding.etUserEmailPass.requestFocus();
+
+        }
+
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    binding.etUserEmail.setText("");
+                    binding.etUserEmailPass.setText("");
+                    finish();
+
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login unsuccessful", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
--- app/src/main/java/com/example/tourmatenewproject/ui/SignUpActivity.java
+++ app/src/main/java/com/example/tourmatenewproject/ui/SignUpActivity.java
@@ -1,16 +1,140 @@
 package com.example.tourmatenewproject.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.tourmatenewproject.R;
+import com.example.tourmatenewproject.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
 
 public class SignUpActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+    private androidx.appcompat.app.AlertDialog alertDialog;
+    private ActivitySignUpBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                userSignUp();
+
+            }
+        });
+
+        binding.tvLoginSU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        showAlertDialog();
+
+    }
+
+    private void showAlertDialog() {
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.custom_alert_dialog, null);
+
+        alertDialog = new androidx.appcompat.app.AlertDialog.Builder(this).setView(view).create();
+        alertDialog.show();
+
+        Button buttonYes = view.findViewById(R.id.btnYes);
+        Button buttonNo = view.findViewById(R.id.btnNo);
+
+        buttonYes.setOnClickListener(view1 -> finish());
+        buttonNo.setOnClickListener(view12 -> alertDialog.cancel());
+
+    }
+
+
+    private void userSignUp() {
+        //String userName = etSignUpUserName.getText().toString().trim().toLowerCase();
+        String email = binding.etEmailAddress.getText().toString().trim();
+        String password = binding.etEmailPassword.getText().toString().trim();
+        String passConfirm = binding.etEmailConfirmPass.getText().toString().trim();
+
+
+        //checking the validity of the email
+        if (email.isEmpty()) {
+            binding.etEmailAddress.setError("Enter an email address");
+            binding.etEmailAddress.requestFocus();
+            return;
+        }
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.etEmailAddress.setError("Enter a valid email address");
+            binding.etEmailAddress.requestFocus();
+            return;
+        }
+
+        //checking the validity of the password
+        if (password.isEmpty()) {
+            binding.etEmailPassword.setError("Enter a password");
+            binding.etEmailPassword.requestFocus();
+        }
+        if (password.length() < 6) {
+            binding.etEmailPassword.setError("minimum length at least 6");
+            binding.etEmailPassword.requestFocus();
+
+        }
+        //checking the validity of Confirm password
+        if (passConfirm.isEmpty()) {
+            //etConfirmPassword.setError("Enter confirm password");
+            Toast.makeText(getApplicationContext(), "Enter confirm password ", Toast.LENGTH_SHORT).show();
+            binding.etEmailConfirmPass.requestFocus();
+        } else if (!passConfirm.equalsIgnoreCase(password)) {
+            //etConfirmPassword.setError("Password not matched");
+            Toast.makeText(getApplicationContext(), "Password not matched ", Toast.LENGTH_SHORT).show();
+            binding.etEmailConfirmPass.requestFocus();
+
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Registration successfully completed ", Toast.LENGTH_SHORT).show();
+                    binding.etEmailAddress.setText("");
+                    binding.etEmailPassword.setText("");
+                    binding.etEmailConfirmPass.setText("");
+                } else {
+                    if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                        Toast.makeText(getApplicationContext(), "User is already registered", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
     }
 }
